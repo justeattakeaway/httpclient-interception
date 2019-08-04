f10e60af902dd3dbdb77f8990de966a5f1542fb5
martincostello <martin@martincostello.com>
2019-08-04 15:05:31 +0100

    Compare JSON implementations
    Compare the two different JSON serializers in the benchmarks.
    Use explicit discard for benchmarks.

--- tests/HttpClientInterception.Benchmarks/InterceptionBenchmarks.cs
+++ tests/HttpClientInterception.Benchmarks/InterceptionBenchmarks.cs
@@ -1,8 +1,12 @@
 ﻿// Copyright (c) Just Eat, 2017. All rights reserved.
 // Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
 
+using System;
 using System.IO;
 using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using BenchmarkDotNet.Attributes;
 using Newtonsoft.Json.Linq;
@@ -15,7 +19,8 @@ namespace JustEat.HttpClientInterception
     {
         private readonly HttpClientInterceptorOptions _options;
         private readonly HttpClient _client;
-        private readonly IGitHub _service;
+        private readonly IGitHub _serviceNewtonsoftJson;
+        private readonly IGitHub _serviceSystemTextJson;
 
         public InterceptionBenchmarks()
         {
@@ -63,29 +68,42 @@ namespace JustEat.HttpClientInterception
                 .WithContentStream(() => File.OpenRead("organization.json"))
                 .RegisterWith(_options);
 
+            var refitSettings = new RefitSettings()
+            {
+                ContentSerializer = new SystemTextJsonContentSerializer(),
+            };
+
 #pragma warning disable CA2000
             _client = _options.CreateHttpClient();
-            _service = RestService.For<IGitHub>(_options.CreateHttpClient("https://api.github.com"));
+            _serviceNewtonsoftJson = RestService.For<IGitHub>(_options.CreateHttpClient("https://api.github.com"));
+            _serviceSystemTextJson = RestService.For<IGitHub>(_options.CreateHttpClient("https://api.github.com"), refitSettings);
 #pragma warning restore CA2000
         }
 
         [Benchmark]
         public async Task GetBytes()
         {
-            await _client.GetByteArrayAsync("https://files.domain.com/setup.exe");
+            _ = await _client.GetByteArrayAsync("https://files.domain.com/setup.exe");
         }
 
         [Benchmark]
         public async Task GetHtml()
         {
-            await _client.GetStringAsync("http://www.google.co.uk/search?q=Just+Eat");
+            _ = await _client.GetStringAsync("http://www.google.co.uk/search?q=Just+Eat");
         }
 
         [Benchmark]
-        public async Task GetJson()
+        public async Task GetJsonNewtonsoftJson()
         {
             string json = await _client.GetStringAsync("https://api.github.com/orgs/justeat");
-            JObject.Parse(json);
+            _ = JObject.Parse(json);
+        }
+
+        [Benchmark]
+        public async Task GetJsonSystemTextJson()
+        {
+            var stream = await _client.GetStreamAsync("https://api.github.com/orgs/justeat");
+            using var document = await JsonDocument.ParseAsync(stream);
         }
 
         [Benchmark]
@@ -97,9 +115,50 @@ namespace JustEat.HttpClientInterception
         }
 
         [Benchmark]
-        public async Task Refit()
+        public async Task RefitNewtonsoftJson()
         {
-            await _service.GetOrganizationAsync("justeat");
+            _ = await _serviceNewtonsoftJson.GetOrganizationAsync("justeat");
+        }
+
+        [Benchmark]
+        public async Task RefitSystemTextJson()
+        {
+            _ = await _serviceSystemTextJson.GetOrganizationAsync("justeat");
+        }
+
+        internal sealed class SystemTextJsonContentSerializer : IContentSerializer
+        {
+            private static readonly MediaTypeHeaderValue _jsonMediaType =
+                new MediaTypeHeaderValue("application/json") { CharSet = Encoding.UTF8.WebName };
+
+            public async Task<T> DeserializeAsync<T>(HttpContent content)
+            {
+                using var stream = await content.ReadAsStreamAsync();
+                return await JsonSerializer.DeserializeAsync<T>(stream);
+            }
+
+            public async Task<HttpContent> SerializeAsync<T>(T item)
+            {
+                var stream = new MemoryStream();
+
+                try
+                {
+                    await JsonSerializer.SerializeAsync(stream, item);
+                    await stream.FlushAsync();
+                    stream.Seek(0, SeekOrigin.Begin);
+
+                    var content = new StreamContent(stream);
+
+                    content.Headers.ContentType = _jsonMediaType;
+
+                    return content;
+                }
+                catch (Exception)
+                {
+                    await stream.DisposeAsync();
+                    throw;
+                }
+            }
         }
     }
 }
