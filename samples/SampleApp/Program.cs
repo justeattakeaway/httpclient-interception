8abd83aee200539b1dff3855ca99ab2a5f978aa3
martincostello <martin@martincostello.com>
2021-08-23 12:10:35 +0100

    Use minimal APIs
    Refactor the sample API to use minimal APIs.

--- samples/SampleApp/Program.cs
+++ samples/SampleApp/Program.cs
@@ -2,18 +2,32 @@
 // Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
 
 using SampleApp.Extensions;
+using SampleApp.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddHttpClients();
 
-builder.Services.AddControllers()
-                .AddJsonOptions((p) => p.JsonSerializerOptions.WriteIndented = true);
-
 var app = builder.Build();
 
-app.UseRouting();
+app.MapGet("/api/repos", async (IConfiguration config, IGitHub github, int? count) =>
+{
+    // Get the configured organization's repositories
+    string organization = config["Organization"];
 
-app.MapDefaultControllerRoute();
+    ICollection<Repository> repositories = await github.GetRepositoriesAsync(organization, count ?? 100);
+
+    // Return the repositories' names
+    var names = new List<string>();
+
+    foreach (var repository in repositories)
+    {
+        names.Add(repository.Name);
+    }
+
+    names.Sort();
+
+    return Results.Json(names, new() { WriteIndented = true });
+});
 
 app.Run();
